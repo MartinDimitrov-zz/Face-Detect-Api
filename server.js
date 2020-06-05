b1c903b8e03fec075bec5bc125a934b16444e683
martin <martin@MacBook-Pro.local>
2020-06-05 15:35:23 +0300

    deploy

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => { res.send(); });
+app.get('/', (req, res) => { res.send('It Works!'); });
 app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db); });
 app.post('/signin', (req, res) => { login.handleLogin(req, res, db, bcrypt); });
 app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt); });
