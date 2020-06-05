dedff77f8753aa2d5d673d33d0ea2b086b297176
martin <martin@MacBook-Pro.local>
2020-06-05 12:59:11 +0300

    facedetector-serv

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const bcrypt = require('bcrypt-nodejs');
+const cors = require('cors');
+const knex = require('knex');
+
+const register = require('./controllers/register');
+const login = require('./controllers/login');
+const image = require('./controllers/image');
+const profile = require('./controllers/profile');
+
+const db = knex({
+    client: 'pg',
+    connection: {
+        host: '127.0.0.1',
+        user: 'martin',
+        password: '',
+        database: 'facedetector'
+    }
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => { res.send(); });
+app.get('/profile/:id', (req, res) => { profile.handleProfile(req, res, db); });
+app.post('/signin', (req, res) => { login.handleLogin(req, res, db, bcrypt); });
+app.post('/register', (req, res) => { register.handleRegister(req, res, db, bcrypt); });
+app.put('/image', (req, res) => { image.handleImage(req, res, db); });
+app.post('/imageurl', (req, res) => { image.handleApi(req, res); });
+
+app.listen(3001, () => { console.log('App is running on port 3001'); });
